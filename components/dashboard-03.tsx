62717a9a7e0ef68e0ed2578dd4a65fb6b35c5d82
James Yang <jamesbun10@gmail.com>
2024-11-17 03:49:43 -0600

    Fixed overlay issue and added dynamic image sizing

--- components/dashboard-03.tsx
+++ components/dashboard-03.tsx
@@ -408,16 +408,13 @@ export default function Dashboard() {
           </Button>
         </header>
         <main className="grid flex-1 gap-4 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-3">
-          <div
-            className="relative hidden flex-col items-start gap-8 md:flex"
-            x-chunk="A settings form a configuring an AI model and messages."
-          >
+          <div className="relative hidden flex-col items-start gap-8 md:flex">
             <form
               className="grid w-full items-start gap-6"
               onSubmit={generateArticle}
             >
-              <fieldset className="grid gap-6 rounded-lg border p-4">
-                <div className="grid gap-3">
+              <fieldset className="grid gap-4 rounded-lg border p-4">
+                <div className="grid gap-2">
                   <Label htmlFor="model">Article Type</Label>
                   <Select onValueChange={(value) => setIntent(value)}>
                     <SelectTrigger
@@ -512,7 +509,7 @@ export default function Dashboard() {
                     </SelectContent>
                   </Select>
                 </div>
-                <div className="grid gap-3">
+                <div className="grid gap-2">
                   <Label htmlFor="length">Article Length</Label>
                   <Select onValueChange={(value) => setLength(value)}>
                     <SelectTrigger
@@ -573,15 +570,16 @@ export default function Dashboard() {
               </fieldset>
 
               {/* Article Prompt Input */}
-              <fieldset className="grid gap-6 rounded-lg border p-4">
+              <fieldset className="grid gap-4 rounded-lg border p-4">
                 <legend className="-ml-1 px-1 text-sm font-medium">
                   {/* Title for fieldset */}
                 </legend>
-                <div className="grid gap-3">
+                <div className="grid gap-2">
                   <Label htmlFor="content">Article Prompt</Label>
-                  <span className="text-sm font-light">
+                  <span className="text-sm font-light italic">
                     {" "}
-                    Include main points and ideas of the article{" "}
+                    Instructions for the content generation - include main
+                    points and ideas for the article{" "}
                   </span>
                   <Textarea
                     id="content"
@@ -607,34 +605,35 @@ export default function Dashboard() {
               </fieldset>
 
               {/* Article Image Section */}
-              <fieldset className="grid gap-6 rounded-lg border p-4">
+              <fieldset className="grid gap-4 rounded-lg border p-4">
                 <Label htmlFor="image-prompt">Article Image</Label>
-                <div className="border rounded-lg relative w-[500px] h-[500px]">
-                  {isImageLoading ? (
-                    <div
-                      className="flex justify-center items-center bg-slate-200"
-                      style={{ width: "100%", height: "100%" }}
-                    >
-                      <div
-                        className="animate-spin rounded-full border-b-2 border-gray-900"
-                        style={{ width: "64px", height: "64px" }}
-                      ></div>
-                    </div>
-                  ) : generatedImageUrl ? (
-                    <Image
-                      src={generatedImageUrl}
-                      alt="Generated Thumbnail"
-                      fill
-                      className="border-4 rounded-lg"
-                    />
-                  ) : (
-                    <Image
-                      src="/abstract-paint.webp"
-                      alt="Placeholder Image"
-                      fill
-                      className="border-4 rounded-lg"
-                    />
-                  )}
+                <div className="flex justify-center">
+                  <div className="border rounded-lg relative w-full aspect-square">
+                    {isImageLoading ? (
+                      <div className="flex justify-center items-center bg-slate-200 w-full h-full">
+                        <div
+                          className="animate-spin rounded-full border-b-2 border-gray-900"
+                          style={{ width: "64px", height: "64px" }}
+                        ></div>
+                      </div>
+                    ) : generatedImageUrl ? (
+                      <Image
+                        src={generatedImageUrl}
+                        alt="Generated Thumbnail"
+                        layout="fill"
+                        objectFit="contain"
+                        className="border-4 rounded-lg"
+                      />
+                    ) : (
+                      <Image
+                        src="/abstract-paint.webp"
+                        alt="Placeholder Image"
+                        layout="fill"
+                        objectFit="contain"
+                        className="border-4 rounded-lg"
+                      />
+                    )}
+                  </div>
                 </div>
                 <div className="grid gap-3">
                   <Textarea
@@ -653,6 +652,7 @@ export default function Dashboard() {
               </fieldset>
             </form>
           </div>
+
           <div className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
             <Badge variant="outline" className="absolute right-3 top-3">
               Output
